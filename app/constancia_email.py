6ddf89d1d918de7f09ccf734dc3c07bb07d5fab8
Mica <58011910+micaelagimenez@users.noreply.github.com>
2021-03-12 15:58:23 -0300

    Update constancia_email.py

--- app/constancia_email.py
+++ app/constancia_email.py
@@ -1,35 +1,11 @@
-from email.mime.text import MIMEText
-from email.mime.application import MIMEApplication
-from email.mime.multipart import MIMEMultipart
-
-import boto
+from django.core.mail import EmailMultiAlternatives
 
 def constancia_email(email):
 
-        msg = MIMEMultipart()
-        msg['Subject'] = 'Constancia Monotributo'
-        msg['From'] = ''
-        msg['To'] = email
-
-        msg.preamble = 'Multipart message.\n'
-
-        # msg body
-        part = MIMEText('')
-        msg.attach(part)
-
-        # pdf
-        part = MIMEApplication(open('constancia.pdf','rb').read())
-        part.add_header('Content-Disposition', 'attachment', filename='constancia.pdf')
-        msg.attach(part)
-
-        #  SES
-        connection = boto.connect_ses(aws_access_key_id=''
-            , aws_secret_access_key='')
-
-        # send
-        result = connection.send_raw_email(msg.as_string()
-            , source=msg['From']
-            , destinations=[msg['To']])
-        
-        print(result)
+    subject, from_email, to = 'Constancia Monotributo', 'braavosi@outlook.es', email
+    text_content = 'Hola! Gracias por usar Gestorando.  Te enviamos el PDF de la Constancia Monotributo que solicistate. Saludos, El equipo de Gestorando'
+    html_content = '<p><strong>Hola!</strong></p> <p> Gracias por usar Gestorando. Te enviamos el PDF de la Constancia Monotributo que solicistate. </p> <p>Saludos,</p> <p>El equipo de Gestorando</p>'
+    msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
 
