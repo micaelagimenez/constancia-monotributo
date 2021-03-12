29ef3224965c40ed50b6c7d3d917666f8ed1a7b6
Mica <58011910+micaelagimenez@users.noreply.github.com>
2021-03-12 16:45:10 -0300

    Update constancia_email.py

--- app/constancia_email.py
+++ app/constancia_email.py
@@ -1,11 +1,15 @@
 from django.core.mail import EmailMultiAlternatives
+from . import views
 
 def constancia_email(email):
 
     subject, from_email, to = 'Constancia Monotributo', 'braavosi@outlook.es', email
     text_content = 'Hola! Gracias por usar Gestorando.  Te enviamos el PDF de la Constancia Monotributo que solicistate. Saludos, El equipo de Gestorando'
     html_content = '<p><strong>Hola!</strong></p> <p> Gracias por usar Gestorando. Te enviamos el PDF de la Constancia Monotributo que solicistate. </p> <p>Saludos,</p> <p>El equipo de Gestorando</p>'
+    
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+    attachment = open('constancia.pdf', 'rb')
+    msg.attach('constancia.pdf', attachment.read(), 'text/pdf')
+    msg.content_subtype = 'html'
     msg.attach_alternative(html_content, "text/html")
     msg.send()
-
